67a8a814e39682339a949d0447b7b4db4cc4b2c9
saurabhs <saurabhpshirsath@gmail.com>
2021-08-10 16:28:30 +0530

    [saurabh]Add: Binary Search Word

---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class BinarySearch
+    {
+        public static void BinarySearchWord(List<string> list)
+        {
+            int mid, start = 0;
+            int end = list.Count - 1;
+            bool result = false;
+            Console.Write("Enter the word to be search : ");
+            string search = Console.ReadLine();
+
+            while (start <= end)
+            {
+                mid = start + (end - start) / 2;
+                int flag = search.CompareTo(list[mid]);
+                if (flag == 0)
+                {
+                    Console.WriteLine("Given word is present in text File!");
+                    result = true;
+                    break;
+                }
+                else if (flag > 0)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            if (!result)
+            {
+                Console.WriteLine("Given word is not present in text File!");
+            }
+
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Algorithms
 {
@@ -9,6 +11,7 @@ namespace Algorithms
             Console.WriteLine("Welcome To Algorithm Programs");
             Console.WriteLine("<<<<<>>>>>>>>>><<<<<<<<>>>>>>");
             Console.WriteLine("1.Permutation of string");
+            Console.WriteLine("2.Binary search word");
             Console.Write("Enter your choice:");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -18,6 +21,12 @@ namespace Algorithms
                     string str = Console.ReadLine();
                     int length = str.Length;
                     Permutation.permute(str, 0, length - 1);
+                    break;
+                case 2:
+                    string filepath = File.ReadAllText(@"D:\AlgorithmPrograms\Algorithms\WordList.txt");
+                    List<string> list = new List<string>(filepath.Split(' '));
+                    list.Sort();
+                    BinarySearch.BinarySearchWord(list);
                     break;
                 default:
                     break;
