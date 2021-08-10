46dafbebb098f1e8e3f52ae983007b96682008bc
saurabhs <saurabhpshirsath@gmail.com>
2021-08-10 15:00:58 +0530

    [saurabh]Add: Recursion And Itretative Method Implementation

---
+++ Algorithms/Permutation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    class Permutation
+    {
+        public static void permute(String str, int start, int end)
+        {
+            if (start == end)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    str = swap(str, start, i);
+                    permute(str, start + 1, end);
+                    str = swap(str, start, i);
+                }
+            }
+        }
+
+        //Swap Characters at position
+        //@return swapped string
+        public static String swap(String a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -7,6 +7,22 @@ namespace Algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Algorithm Programs");
+            Console.WriteLine("<<<<<>>>>>>>>>><<<<<<<<>>>>>>");
+            Console.WriteLine("1.Permutation of string");
+            Console.Write("Enter your choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter The  String : ");
+                    string str = Console.ReadLine();
+                    int length = str.Length;
+                    Permutation.permute(str, 0, length - 1);
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
